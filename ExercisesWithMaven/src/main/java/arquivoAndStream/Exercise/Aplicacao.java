e8a8c4dc2001fa5a1ddf136cdf23dda3588058a7
Roger <rogerio.alonso@soulasalle.com.br>
2021-10-16 13:37:11 -0300

    Questão 1

--- ExercisesWithMaven/src/main/java/arquivoAndStream/Exercise/Aplicacao.java
+++ ExercisesWithMaven/src/main/java/arquivoAndStream/Exercise/Aplicacao.java
@@ -1,7 +1,15 @@
 package arquivoAndStream.Exercise;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static arquivoAndStream.Exercise.Premiacao.*;
 
@@ -9,19 +17,48 @@ public class Aplicacao {
 
     public static void main(String[] args) {
 
+        Aplicacao app = new Aplicacao();
+
+        List<Premiacao> oscarMale = new ArrayList<>();
+        List<Premiacao> oscarFemale = new ArrayList<>();
+
         //Lendo os arquivos csv
-        List<Premiacao> premiosMale = new ArrayList<>();
-        List<Premiacao> premiosFemale = new ArrayList<>();
-        readFile(premiosMale, "oscar_male.csv");
-        readFile(premiosFemale, "oscar_female.csv");
+        app.readFile(oscarMale, "oscarMale.csv");
+        app.readFile(oscarFemale, "oscarFemale.csv");
 
         //Guardando os dados separado por pessoa
         //List<Ator> premiados = new ArrayList<>();
         //addAtor(premiados, premiosMale);
         //addAtor(premiados, premiosFemale);
 
-        System.out.printf("\n\nO ator mais jovem foi %s.\n", youngActor(premiosMale));
-        atrizMaisPremiada(premiosFemale);
+        //Questão 1
+        System.out.printf("\n\nO ator mais jovem foi: ");
+        atorMaisJovem(oscarMale);
 
+        //Questão 2
+        atrizMaisPremiada(oscarFemale);
+
+    }
+
+    private void readFile(List<Premiacao> premios, String filename){
+
+        String filepath = getFilepathFromResourceAsStream(filename);
+        try (Stream<String> stream = Files.lines(Path.of(filepath))) {
+            List<String> lines = stream.skip(1).collect(Collectors.toList());
+            for (String line : lines) {
+                String[] info = line.split("; ");
+                Premiacao newPremio = new Premiacao(Integer.parseInt(info[0]),Integer.parseInt(info[1]), Integer.parseInt(info[2]),
+                        info[3], info[4]);
+                premios.add(newPremio);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getFilepathFromResourceAsStream(String fileName) {
+        URL url = getClass().getClassLoader().getResource(fileName);
+        File file = new File(url.getFile());
+        return file.getPath();
     }
 }
--- ExercisesWithMaven/src/main/java/arquivoAndStream/Exercise/Premiacao.java
+++ ExercisesWithMaven/src/main/java/arquivoAndStream/Exercise/Premiacao.java
@@ -4,8 +4,11 @@ import lombok.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,61 +25,29 @@ public class Premiacao {
     private String name;
     private String movie;
 
-    public static void readFile(List premios, String filename){
-
-        String folder = "/Documentos/LetsCode/LetsCode_Exercises/ExercisesWithMaven";
-        try (Stream<String> stream = Files.lines(Paths.get(System.getProperty("user.home") + folder, filename))) {
-            List<String> lines = stream.collect(Collectors.toList());
-            for (String line : lines) {
-                String[] info = line.split("; ");
-                Premiacao newPremio = new Premiacao(Integer.parseInt(info[0]),Integer.parseInt(info[1]), Integer.parseInt(info[2]),
-                        info[3], info[4]);
-                premios.add(newPremio);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static Premiacao fromLine(String line) {
+        String[] split = line.split("; ");
+        return new Premiacao(
+                Integer.parseInt(split[0]),
+                Integer.parseInt(split[1]),
+                Integer.parseInt(split[2]),
+                split[3],
+                split[4]
+        );
     }
 
-    public static String youngActor(List<Premiacao> premios){
 
-        String nome = "Desconhecido";
-        int maisNovo = 150;
 
-        for(Premiacao info : premios){
-            int idadeAtor = info.getAge();
-            if(idadeAtor < maisNovo){
-                maisNovo = idadeAtor;
-                nome = info.getName();
-            }
-        }
+    public static void atorMaisJovem(List<Premiacao> premios){
 
-        return nome;
+        Optional<Premiacao> premiacaoOptional = premios.stream()
+                .min(Comparator.comparingInt(p -> p.getAge()));
+        premiacaoOptional.ifPresent(p -> System.out.println(p.getName()));
     }
 
     public static void atrizMaisPremiada(List<Premiacao> premios){
-        String nome = "Desconhecido";
-        int qtdMax = 0;
 
-        for(Premiacao num:premios){
-            int qtd = 0;
 
-            for(Premiacao i:premios) {
-                String aux1 = i.getName();
-                String aux2 = num.getName();
-                if (aux1.equals(aux2)) {
-                    qtd++;
-                }
-            }
-
-            if(qtd >= qtdMax){
-                nome = num.getName();
-                qtdMax = qtd;
-            }
-            qtd = 0;
-        }
-
-        System.out.println(nome);
     }
 }
 
